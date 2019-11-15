256670aadcda5dddfbc6564c13bd1937c926e7d4
Jackie Ouzman <jackie.ouzman@csiro.au>
2019-11-15 17:07:29 +1030

    ok its working - sort of not happy with the values the dist is showing

--- MC_app.R
+++ MC_app.R
@@ -7,10 +7,16 @@ library(purrr)
 
 #####################################################################################################################################
 ui <- fluidPage(
-  numericInput("n_obs_input", label = h3("Number of observation"), value = 1),
-  numericInput("mean_input", label = h3("mean yield"), value = 1),
-  numericInput("sd_input", label = h3("SD yield"), value = 1),
-
+  numericInput("area", label = h3("Area of production ha"), value = 400),
+  numericInput("port_price", label = h3("port price $/t"), value = 250),
+  numericInput("wheat_yld", label = h3("wheat yield t/ha"), value = 2.0),
+  numericInput("N_applied", label = h3("N applied kg/ha"), value = 200),
+  numericInput("cost_N", label = h3("cost of N $/t"), value = 560),
+  numericInput("variable_costs", label = h3("variable costs $/ha"), value = 185),
+  
+  #1a. collect base farm values:
+  tableOutput("table1"),
+  
   # 1. collect parameter grids in list display it: 
   #tableOutput("value"),
   
@@ -33,18 +39,27 @@ server <- function(input, output, session) {
   ################   reactive elements   #####################
   ############################################################
   
+  ###############   Farm and crop values ###########################
+  
+  # 1a. function that collect base farm values:
+  making_base_farm <- reactive({
+    #browser()
+    function_making_base_farm(input$port_price,
+                              input$wheat_yld, 
+                              input$area, 
+                              input$N_applied,
+                              input$cost_N,
+                              input$variable_costs) 
+    
+  })
   
   # 1. collect parameter grids in list and make it reactive:
   #the parameter setting will now be hard coded
-  n_obs_input <- rep(seq(2,5,by = 0.2), 10) #this is the rough max and min with number of samples
+  #n_obs_input <- rep(seq(2,5,by = 0.2), 10) #this is the rough max and min with number of samples
+  n_obs_input <- 1000
   shape = 5 #the shape controls the shape of the distrbution, smaller numbers will make the distribution skew to the left
   scale = 5 #controls the varaibility of the data, sharp curves with small tails or flats curves with long tails smaller values give you flatter curves
-  # mean_input <- 1
-  # sd_input <- 1
-  # param_list <- reactive({
-  #   param_list = list(n = n_obs_input, "loc" = mean_input, "scale" = sd_input)
-  #   })
-  
+ 
   param_list <- reactive({
     param_list = list("n" = n_obs_input, "shape" = shape, "scale" = scale)
   })
@@ -52,7 +67,7 @@ server <- function(input, output, session) {
   # 2. use MonteCarlo simulations and make the output reactive: Note I have hard coded the number of reps and I am returning a df
   ## Note I am having trouble accessing the function jax2 - needs to be run first??
   MC_results <- reactive({
-    MC_result <- MonteCarlo(func=jaxs2, nrep=10, param_list=param_list(), ncpus=1)
+    MC_result <- MonteCarlo(func=jaxs2, nrep=1000, param_list=param_list(), ncpus=1)
     MC_result_df<-MakeFrame(MC_result)
     MC_result_df ### do I need this line to return df?
     })  
@@ -66,6 +81,11 @@ server <- function(input, output, session) {
   ############################################################
   ################       outputs         #####################
   ############################################################ 
+  
+  #1a. 
+  output$table1 <- renderTable({
+    making_base_farm()
+  })
   
   # 1. collect parameter grids in list display it: 
   output$value <- renderTable({
@@ -85,7 +105,7 @@ server <- function(input, output, session) {
   # 4. plot the results
   output$plot <- renderPlot({
     MC_results() %>% 
-      ggplot(aes(x = yeild_gain)) + 
+      ggplot(aes(x = base_yld)) + 
       geom_density()
       
   })
@@ -100,13 +120,15 @@ server <- function(input, output, session) {
   jaxs2<-function(n,shape,scale){
     
     #this sets up the input yield distribution
-    sample <- rllogis(n, shape , scale)
+    #set.seed(16)
+    base_yld1 <- (rllogis(n, shape , scale))
+    base_yld <- base_yld1 * 5 #this step convert my distrubution into values I want
    
     #Now I can acess values from the yield distribution and run a calulation
     #It is important to access one value at a time to run the calulations on - here I have pulled out random value from the yield distrubution
-    yeild_gain<- 1 * sample (sample, size=1) #this works
+    base_yld <-  (sample (base_yld, size=1))  
     
-    return(list("yeild_gain" = yeild_gain))
+    return(list("base_yld" = base_yld))
   }
   
   # 2. function that will run some summary stats on MC_df results:
@@ -121,13 +143,13 @@ server <- function(input, output, session) {
     
     # B. Use step A in dplyr pipe workflow
     MC_result_df_summary_stats1 <- MC_results %>% 
-      summarize_at(vars(yeild_gain), funs(!!!p_funs))
+      summarize_at(vars(base_yld), funs(!!!p_funs))
     
     # C. cal other summary stats using dplyr workflow
     MC_result_df_summary_stats2 <- MC_results %>%
-      summarise(IQR = IQR(yeild_gain),
-                mean = mean(yeild_gain),
-                median = median(yeild_gain))
+      summarise(IQR = IQR(base_yld),
+                mean = mean(base_yld),
+                median = median(base_yld))
     # D. Join together summary stats part B and C together
     
     
@@ -135,6 +157,22 @@ server <- function(input, output, session) {
     MC_result_df_summary_stats
   }
   
+  
+  # 1a. function that collect base farm values:
+   function_making_base_farm <-  function(port_price, wheat_yld, area, 
+                                         N_applied, cost_N, variable_costs){
+     
+    base_farm <- data.frame(site_number = "xxxx", #user defined
+                            year = 1:5,
+                            crop = "wheat",
+                            port_price =  port_price,
+                            wheat_yld = wheat_yld,
+                            area = area,
+                            N_applied = N_applied,
+                            cost_N = cost_N,
+                            variable_costs = variable_costs)
+    return(base_farm)
+  }
   
 }
 shinyApp(ui, server)
